1c0dce80a9049e9a66fae53044b32438bcf868e5
SenciZ <zenkovic88@gmail.com>
2021-06-21 16:07:21 -0500

    Add menu items

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,30 @@
 
 const menu = function(){
-  let bgCover = document.createElement("div");
-  bgCover.style.width = "100vw";
-  bgCover.style.height = "100vh";
-  bgCover.classList.add("bgCover");
   let mainContainer = document.createElement("main");
   mainContainer.classList.add("mainContainer");
+  mainContainer.innerHTML=`<div class="menuDiv"><div class="menuItem">
+  <h3 class="title">Seasonal Raw Oysters</h3>
+  <h4 class="price">Doz 36/Half 18/ea 3</h4>
+  <p class="itemdescription">Served with horseradish, cocktail and mignonette sauces.</p>
+</div>
+<div class="menuItem">
+  <h3 class="title">Classic Shrimp Cocktail</h3>
+  <h4 class="price">12.99</h4>
+  <p class="itemdescription">Poached shrimp served chilled with house cocktail sauce and lemon.</p>
+</div>
+<div class="menuItem">
+  <h3 class="title">Fried Calamari</h3>
+  <h4 class="price">13.99</h4>
+  <p class="itemdescription">Peppadew peppers, rouille and cocktail sauce</p>
+</div>
+<div class="menuItem">
+  <h3 class="title">Poutine</h3>
+  <h4 class="price">15.99</h4>
+  <p class="itemdescription">Braised beef, caramelized onion, mushrooms, Milton Creamery cheese curds and demi-glace over hand-cut French fries</p>
+</div></div>`
   let content = document.querySelector("#content");
-  let aboutUs = document.createElement("h1");
-  aboutUs.classList.add("aboutText");
-  aboutUs.textContent = "Menu";
-  content.appendChild(bgCover);
-  bgCover.appendChild(mainContainer);
-  mainContainer.appendChild(aboutUs);
-
+  content.appendChild(mainContainer);
 };
 
 export  { menu };
+
